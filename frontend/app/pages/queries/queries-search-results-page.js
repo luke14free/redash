c12ab4d3877d7099a9a988034f255d8efc2daffc
luke14free <luca.giacomel@gmail.com>
2016-11-23 19:12:57 +0100

    First UI work

--- frontend/app/pages/queries/queries-search-results-page.js
+++ frontend/app/pages/queries/queries-search-results-page.js
@@ -1,23 +1,48 @@
 import moment from 'moment';
-import { isString } from 'underscore';
+import { isString, any, keys } from 'underscore';
 
 import { Paginator } from '../../utils';
 import template from './queries-search-results-page.html';
 
-function QuerySearchCtrl($location, $filter, currentUser, Events, Query) {
+function QuerySearchCtrl($location, $filter, currentUser, Events, Query, AlertDialog, toastr) {
   // $scope.$parent.pageTitle = 'Queries Search';
 
+  this.selected = {};
   this.term = $location.search().q;
   this.paginator = new Paginator([], { itemsPerPage: 20 });
+  this.queries = {};
 
-  Query.search({ q: this.term }, (results) => {
-    const queries = results.map((query) => {
-      query.created_at = moment(query.created_at);
-      return query;
+  this.anySelected = () => any(this.selected);
+
+  this.doArchiveAll = () => {
+    keys(this.selected).forEach((id) => {
+      Query.delete({ id }, this.loadSearchResults, () => {
+        toastr.error('Query could not be archived.');
+      });
     });
+    this.selected = {};
+  };
 
-    this.paginator.updateRows(queries);
-  });
+  this.archiveAll = () => {
+    const title = 'Archive Selected Queries';
+    const message = 'Are you sure you want to archive selected queries?<br/> All alerts and dashboard widgets created with the selected visualizations will be deleted.';
+    const confirm = { class: 'btn-warning', title: 'Archive' };
+    AlertDialog.open(title, message, confirm).then(this.doArchiveAll);
+  };
+
+  this.loadSearchResults = () => {
+    Query.search({ q: this.term }, (results) => {
+      const queries = results.map((query) => {
+        this.queries[query.id] = query;
+        query.created_at = moment(query.created_at);
+        return query;
+      });
+
+      this.paginator.updateRows(queries);
+    });
+  };
+
+  this.loadSearchResults();
 
   this.search = () => {
     if (!isString(this.term) || this.term.trim() === '') {
